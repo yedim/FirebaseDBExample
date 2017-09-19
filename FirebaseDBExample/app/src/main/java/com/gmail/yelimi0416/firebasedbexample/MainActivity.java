610a11520403d7da96ff696852f5378f9af65387
yedim <yelimi0416@gmail.com>
2017-09-19 20:53:20 +0900

    #5 웹 브라우저로 링크 띄우기 구현

--- FirebaseDBExample/app/src/main/java/com/gmail/yelimi0416/firebasedbexample/MainActivity.java
+++ FirebaseDBExample/app/src/main/java/com/gmail/yelimi0416/firebasedbexample/MainActivity.java
@@ -1,13 +1,16 @@
 package com.gmail.yelimi0416.firebasedbexample;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView mNameTextView;
     private TextView mGithubTextView;
@@ -27,8 +30,17 @@ public class MainActivity extends AppCompatActivity {
                 .centerCrop()
                 .resize(100,100)
                 .into(mProfileImageView);
+    }
 
-
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.profile_github:
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri uri = Uri.parse( mGithubTextView.getText().toString() );
+                intent.setData(uri);
+                startActivity(intent);
+                break;
+        }
     }
 }
